02a3953b0afc53df7e45f2d7c3a891c30347e375
Randy Esplin <Randy.Esplin@ECInternet.com>
2024-05-17 10:03:13 -0700

    ProductDataProcessorInterface - Add more methods

--- Api/ProductDataProcessorInterface.php
+++ Api/ProductDataProcessorInterface.php
@@ -9,5 +9,21 @@ namespace ECInternet\RAPIDWebSync\Api;
 
 interface ProductDataProcessorInterface
 {
+    public function beforeImport();
+
+    public function startImport();
+
+    public function processColumnList();
+
+    public function processItemBeforeId(array $productData, string $sku);
+
     public function process(array $productData, string $sku, int $productId);
+
+    public function preprocessItemAfterId(array $productData, string $sku, int $productId);
+
+    public function processItemAfterId(array $productData, string $sku, int $productId);
+
+    public function endImport();
+
+    public function afterImport();
 }
