1c73e178bbc847ad6387427f1e451644a67543e3
Randy Esplin <randy.esplin@gmail.com>
2025-12-16 15:55:35 -0800

    StoreWebsiteTest - Start building out

--- Test/Integration/Helper/StoreWebsiteTest.php
+++ Test/Integration/Helper/StoreWebsiteTest.php
@@ -7,7 +7,30 @@ declare(strict_types=1);
 
 namespace ECInternet\RAPIDWebSync\Test\Integration\Helper;
 
+use Magento\TestFramework\Helper\Bootstrap;
+use ECInternet\RAPIDWebSync\Helper\StoreWebsite;
+
+/**
+ * @magentoDbIsolation disabled
+ */
 class StoreWebsiteTest extends \PHPUnit\Framework\TestCase
 {
+    private $storeWebsite;
 
+    protected function setUp(): void
+    {
+        // Load classes from here
+        $objectManager = Bootstrap::getObjectManager();
+
+        // Test class
+        $this->storeWebsite = $objectManager->get(StoreWebsite::class);
+    }
+
+    public function testGetStoreIds()
+    {
+        $result = $this->storeWebsite->getStoreIds();
+        $this->assertIsArray($result);
+        $this->assertContainsOnly('int', $result);
+        $this->assertTrue(count($result) >= 1);
+    }
 }
